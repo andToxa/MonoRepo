1527c6cec1ed7ec69f48f96df69fe7f1bd0f2711
andToxa <andToxa@gmail.com>
2020-11-15 17:01:26 +0700

    + Common.NET

---
+++ Common.NET/src/Infrastructure.Common.Extensions.Logging/SerilogExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Infrastructure.Common.Extensions.Logging
+{
+    public static class SerilogExtensions
+    {
+        public static IHostBuilder UseSerilogExtension(this IHostBuilder app)
+        {
+            return app.UseSerilog((context, configuration) =>
+            {
+                configuration
+                    .Enrich.FromLogContext();
+            });
+        }
+    }
+}
