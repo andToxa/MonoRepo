e7faab010f4bd4eb8a7f59725815016c710853f5
andToxa <andToxa@gmail.com>
2021-01-08 21:43:35 +0700

    SANDBOX-T-1 ReplacePropertyNamesByValues refactoring

--- Common.NET/src/Application.Common/Extensions/StringExtensions.cs
+++ Common.NET/src/Application.Common/Extensions/StringExtensions.cs
@@ -6,23 +6,23 @@ namespace Application.Common.Extensions
     public static class StringExtensions
     {
         /// <summary>Замена шаблона {ClassName.PropertyName} на имя класса и значение свойства</summary>
-        /// <param name="sourceString">Исходная строка</param>
-        /// <param name="sourceObjects">Объекты, имена и значения свойств которых используются для подстановки</param>
+        /// <param name="string">Исходная строка</param>
+        /// <param name="objects">Объекты, имена и значения свойств которых используются для подстановки</param>
         /// <returns>Строка-результат</returns>
-        public static string ReplacePropertyNamesByValues(this string sourceString, params object[] sourceObjects)
+        public static string ReplacePropertyNamesByValues(this string @string, params object[] objects)
         {
-            foreach (var sourceObject in sourceObjects)
+            foreach (var @object in objects)
             {
-                foreach (var property in sourceObject.GetType().GetProperties())
+                foreach (var property in @object.GetType().GetProperties())
                 {
-                    sourceString = Regex.Replace(
-                        input: sourceString,
-                        pattern: $"{{{sourceObject.GetType().Name}.{property.Name}}}",
-                        replacement: property.GetValue(sourceObject)?.ToString() ?? string.Empty);
+                    @string = Regex.Replace(
+                        input: @string,
+                        pattern: $"{{{@object.GetType().Name}.{property.Name}}}",
+                        replacement: property.GetValue(@object)?.ToString() ?? string.Empty);
                 }
             }
 
-            return sourceString;
+            return @string;
         }
     }
 }
