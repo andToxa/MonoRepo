43a2c766c480d91dc4e7e0c0e30e6cf80e7b0647
andToxa <andToxa@gmail.com>
2022-06-20 13:57:03 +0700

    refactor: Rearrange projects

---
+++ Template.NET/src/SharedKernel/SharedKernel.Domain/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using SharedKernel.Domain.ValueObjects;
+
+namespace SharedKernel.Domain.Entities
+{
+    /// <summary>Доменная сущность</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
+    public interface IEntity<T>
+    {
+        /// <summary>Идентификатор сущности</summary>
+        public EntityId<T> Id { get; }
+    }
+}
