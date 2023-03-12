7029c7944ccb1522bb919a649e2cc6a71beb93f6
andToxa <andToxa@gmail.com>
2023-03-12 19:50:46 +0700

    feat(uber-popug-tasks api): add keycloak auth extension

--- SandBox.NET/src/UberPopug/Tasks/UberPopug.Tasks.WebAPI/Program.cs
+++ SandBox.NET/src/UberPopug/Tasks/UberPopug.Tasks.WebAPI/Program.cs
@@ -1,73 +1,15 @@
-﻿using Common.WebAPI.Extensions.Swagger;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Common.WebAPI.Extensions.Keycloak;
+using Common.WebAPI.Extensions.Swagger;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json.Serialization;
-using System.Threading;
-using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
 
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
-var openidConfiguration = await OpenIdConnectConfigurationRetriever.GetAsync("http://keycloak8080.localhost:8080/realms/uber-popug/.well-known/openid-configuration", new HttpDocumentRetriever() { RequireHttps = false }, CancellationToken.None);
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-    {
-        options.Authority = "http://keycloak8080.localhost:8080/realms/uber-popug";
-        options.Audience = "uber-popug-tasks";
-        options.RequireHttpsMetadata = false; // todo только при разработке
-        options.MetadataAddress = "http://keycloak8080.localhost:8080/realms/uber-popug/.well-known/openid-configuration";
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ValidateAudience = false,
-            ValidAudience = "account",
-
-            ValidateIssuer = true,
-            ValidIssuers = new[] { "http://keycloak8080.localhost:8080/realms/uber-popug" },
-
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKeys = openidConfiguration.SigningKeys,
-
-            RequireExpirationTime = true,
-            ValidateLifetime = true,
-            RequireSignedTokens = true,
-        };
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = async context => { await Task.CompletedTask; },
-            OnForbidden = context =>
-            {
-                _ = context;
-                return Task.CompletedTask;
-            },
-            OnAuthenticationFailed = context =>
-            {
-                context.NoResult();
-
-                context.Response.StatusCode = 401;
-                context.Response.ContentType = "text/plain";
-
-                return context.Response.WriteAsync(
-                    builder.Environment.IsDevelopment()
-                        ? context.Exception.ToString() // todo пустой ответ
-                        : "An error occured processing your authentication.");
-            }
-        };
-    });
+builder.Services.AddKeycloakAuthentication("http://keycloak.local.gd:8080//realms/uber-popug");
 
 builder.Services.AddAuthorization(options =>
 {
