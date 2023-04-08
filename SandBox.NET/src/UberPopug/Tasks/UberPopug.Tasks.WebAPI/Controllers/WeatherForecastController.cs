fe2e5d16871498d7f644c33aaefa2e06bcd042fd
andToxa <andToxa@gmail.com>
2023-04-08 19:58:03 +0700

    feat(uber-popug-tasks): add Marten proof of concept

--- SandBox.NET/src/UberPopug/Tasks/UberPopug.Tasks.WebAPI/Controllers/WeatherForecastController.cs
+++ SandBox.NET/src/UberPopug/Tasks/UberPopug.Tasks.WebAPI/Controllers/WeatherForecastController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Common.Domain.ValueObjects;
+using Marten;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Security.Claims;
+using System;
+using System.Threading.Tasks;
+using Task = UberPopug.Tasks.Domain.Task;
 
 namespace UberPopug.Tasks.WebAPI.Controllers;
 
@@ -22,26 +26,57 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IDocumentStore _documentStore;
 
     /// <summary>
     /// Конструктор <see cref="WeatherForecastController"/>
     /// </summary>
     /// <param name="logger"><see cref="ILogger{TCategoryName}"/></param>
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    /// <param name="documentStore"><see cref="IDocumentStore"/></param>
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IDocumentStore documentStore)
     {
-        _logger = logger;
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _documentStore = documentStore ?? throw new ArgumentNullException(nameof(documentStore));
     }
 
     /// <summary>
     /// Получение данных
     /// </summary>
+    /// <param name="guid"><see cref="Guid"/></param>
     /// <remarks>
     /// Пример запроса
     /// </remarks>
-    /// <returns><see cref="ClaimsPrincipal"/></returns>
-    [HttpGet]
-    public ActionResult<ClaimsPrincipal> Get()
+    /// <returns><see cref="UberPopug.Tasks.Domain.Task"/></returns>
+    [HttpGet("{guid:guid}")]
+    [AllowAnonymous]
+    public async Task<ActionResult> GetAsync(Guid guid)
     {
-        return HttpContext.User;
+        await using var session = _documentStore.QuerySession();
+        var existing = await session
+            .Query<UberPopug.Tasks.Domain.Task>()
+            .SingleAsync(x => x.Guid == guid);
+
+        return Ok(existing.TaskId);
+    }
+
+    /// <summary>
+    /// Создание задач
+    /// </summary>
+    /// <returns><see cref="ActionResult"/></returns>
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<ActionResult> CreateTaskAsync()
+    {
+        await using (var session = _documentStore.LightweightSession())
+        {
+            var user = new UberPopug.Tasks.Domain.Task(new Id<Task>());
+            session.Store(user);
+
+            await session.SaveChangesAsync();
+        }
+
+        return Ok();
     }
 }
